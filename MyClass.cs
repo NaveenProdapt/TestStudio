959e83e8f6a136e9d57613367cc3ec1767018f96
Naveenkumar <naveenkumar.r@prodapt.com>
2019-09-18 16:31:50 +0530

    Add files via upload

---
+++ MyClass.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: sumathi.m
+ * Date: 18-09-2019
+ * Time: 14:39
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+
+namespace ClassLib
+{
+	/// <summary>
+	/// Description of MyClass.
+	/// </summary>
+	public class MyClass
+	{
+		public static string username = "mathu@gmail.com";
+		
+		public static string method1(string name){
+			name = "mathu1@gmail.com";
+			return name;
+		}
+	}
+}
